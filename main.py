dbf53a46c8429b5c6a12003f62a303abcf04dec4
Enrico Doretto <enricodoretto@MBP-di-Enrico-2.lan>
2020-11-25 15:03:41 +0100

    Organized in folders and files

--- main.py
+++ main.py
@@ -1,255 +1,78 @@
-from functools import total_ordering, reduce
-from collections import defaultdict
-from math import log
-import csv
-import re
-
 import pickle
+from functools import total_ordering, reduce
+import csv
+import nltk
+import time
+from term_operations import DataDescription
+from term_operations import tokenize
+from trie import TrieNode
+from trie import add
+from trie import find_prefix
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
-@total_ordering
-class Posting:
 
-    def __init__(self, docID, tf):
-        self._docID = docID
-        self._tf = tf  # Term frequency
-
-    def get_from_corpus(self, corpus):
-        return corpus[self._docID]
-
-    def __eq__(self, other):
-        """Perform the comparison between this posting and another one.
-        Since the ordering of the postings is only given by their docID,
-        they are equal when their docIDs are equal.
-        """
-        return self._docID == other._docID
-
-    def __gt__(self, other):
-        """As in the case of __eq__, the ordering of postings is given
-        by the ordering of their docIDs
-        """
-        return self._docID > other._docID
-
-    def __repr__(self):
-        return str(self._docID) + " [" + str(self._tf) + "]"
-
-    def __hash__(self):
-        return hash(self._docID)
-
-
-class PostingList:
-
-    def __init__(self):
-        self._postings = []
-
-    @classmethod
-    def from_docID(cls, docID, tf):
-        """ A posting list can be constructed starting from
-        a single docID.
-        """
-        plist = cls()
-        plist._postings = [Posting(docID, tf)]
-        return plist
-
-    @classmethod
-    def from_posting_list(cls, postingList):
-        """ A posting list can also be constructed by using another
-        """
-        plist = cls()
-        plist._postings = postingList
-        return plist
-
-    def merge(self, other):
-        """Merge the other posting list to this one in a desctructive
-        way, i.e., modifying the current posting list. This method assume
-        that all the docIDs of the second list are higher than the ones
-        in this list. It assumes the two posting lists to be ordered
-        and non-empty. Under those assumptions duplicate docIDs are
-        discarded
-        """
-        i = 0
-        last = self._postings[-1]
-        while (i < len(other._postings) and last == other._postings[i]):
-            last._tf += other._postings[i]._tf
-            i += 1
-        self._postings += other._postings[i:]
-
-    def intersection(self, other):
-        """Returns a new posting list resulting from the intersection
-        of this one and the one passed as argument.
-        """
-        intersection = []
-        i = 0
-        j = 0
-        while (i < len(self._postings) and j < len(other._postings)):
-            if (self._postings[i] == other._postings[j]):
-                intersection.append(self._postings[i])
-                i += 1
-                j += 1
-            elif (self._postings[i] < other._postings[j]):
-                i += 1
-            else:
-                j += 1
-        return PostingList.from_posting_list(intersection)
-
-    def union(self, other):
-        """Returns a new posting list resulting from the union of this
-        one and the one passed as argument.
-        """
-        union = []
-        i = 0
-        j = 0
-        while (i < len(self._postings) and j < len(other._postings)):
-            if (self._postings[i] == other._postings[j]):
-                union.append(self._postings[i])
-                i += 1
-                j += 1
-            elif (self._postings[i] < other._postings[j]):
-                union.append(self._postings[i])
-                i += 1
-            else:
-                union.append(other._postings[j])
-                j += 1
-        for k in range(i, len(self._postings)):
-            union.append(self._postings[k])
-        for k in range(j, len(other._postings)):
-            union.append(other._postings[k])
-        return PostingList.from_posting_list(union)
-
-    def not_query(self):
-        notposting = [];
-        pass
-
-    def get_from_corpus(self, corpus):
-        return list(map(lambda x: x.get_from_corpus(corpus), self._postings))
-
-    def __len__(self):
-        return len(self._postings)
-
-    def __iter__(self):
-        return iter(self._postings)
-
-    def __repr__(self):
-        return ", ".join(map(str, self._postings))
-
-
-def normalize(text):
-    """ A simple funzion to normalize a text.
-    It removes everything that is not a word, a space or an hyphen
-    and downcases all the text.
-    """
-    no_punctuation = re.sub(r'[^\w^\s^-]', '', text)
-    downcase = no_punctuation.lower()
-    return downcase
-
-
-def tokenize(movie):
-    """ From a movie description returns a posting list of all
-    tokens present in the description.
-    """
-    text = normalize(movie.description)
-    return list(text.split())
-
-
-class ImpossibleMergeError(Exception):
-    pass
-
-
-@total_ordering
-class Term:
-
-    def __init__(self, term, docID, tf):
-        self.term = term
-        self.posting_list = PostingList.from_docID(docID, tf)
-
-    def merge(self, other):
-        """Merge (destructively) this term and the corresponding posting list
-        with another equal term and its corrsponding posting list.
-        """
-        if (self.term == other.term):
-            self.posting_list.merge(other.posting_list)
-        else:
-            raise ImpossibleMergeError()
-
-    def __eq__(self, other):
-        return self.term == other.term
-
-    def __gt__(self, other):
-        return self.term > other.term
-
-    def __repr__(self):
-        return self.term + ": " + repr(self.posting_list)
-
-
-class InvertedIndex:
-
-    def __init__(self):
-        self._dictionary = []  # A collection of terms
-        self._N = 0  # Number of documents in the collection
-
-    @classmethod
-    def from_corpus(cls, corpus):
-        # Here we "cheat" by using python dictionaries
-        intermediate_dict = {}
-        for docID, document in enumerate(corpus):
-            tokens = tokenize(document)
-            for token in tokens:
-                term = Term(token, docID, 1)
-                try:
-                    intermediate_dict[token].merge(term)
-                except KeyError:
-                    intermediate_dict[token] = term
-            # To observe the progress of our indexing.
-            if (docID % 1000 == 0):
-                print(str(docID), end='...')
-        idx = cls()
-        idx._N = len(corpus)
-        idx._dictionary = sorted(intermediate_dict.values())
-        return idx
-
-    def __getitem__(self, key):
-        for term in self._dictionary:
-            if term.term == key:
-                return term.posting_list
-        raise KeyError
-
-    def __repr__(self):
-        return "A dictionary with " + str(len(self._dictionary)) + " terms"
-
-
-class MovieDescription:
-
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-
-    def __repr__(self):
-        return self.title  # + "\n" + self.description + "\n"
-
-
-def read_movie_descriptions():
+def read_data_descriptions():
     filename = '../../Google Drive/Università/Corsi/Materiale corsi quinto anno/Information retrieval and data visualization/Lecture 3/MovieSummaries/plot_summaries.txt'
-    movie_names_file = '../../Google Drive/Università/Corsi/Materiale corsi quinto anno/Information retrieval and data visualization/Lecture 3/MovieSummaries/movie.metadata.tsv'
-    with open(movie_names_file, 'r') as csv_file:
-        movie_names = csv.reader(csv_file, delimiter='\t')
+    data_names_file = '../../Google Drive/Università/Corsi/Materiale corsi quinto anno/Information retrieval and data visualization/Lecture 3/MovieSummaries/movie.metadata.tsv'
+    with open(data_names_file, 'r') as csv_file:
+        data_names = csv.reader(csv_file, delimiter='\t')
         names_table = {}
-        for name in movie_names:
+        for name in data_names:
             names_table[name[0]] = name[2]
     with open(filename, 'r') as csv_file:
         descriptions = csv.reader(csv_file, delimiter='\t')
         corpus = []
         for desc in descriptions:
             try:
-                movie = MovieDescription(names_table[desc[0]], desc[1])
-                corpus.append(movie)
+                #coppia titolo-testo
+                corpelement = DataDescription(names_table[desc[0]], desc[1])
+                corpus.append(corpelement)
             except KeyError:
                 # We ignore the descriptions for which we cannot find a title
                 pass
         return corpus
 
+class InvertedIndex:
+    def __init__(self):
+        self._root = TrieNode  # A trie of terms
+        self._N = 0  # Number of documents in the collection
+
+    @classmethod
+    def from_corpus(cls, corpus):
+        root1 = TrieNode('*')
+
+        # Here we "cheat" by using python dictionaries
+        for docID, document in enumerate(corpus):
+            tokens = tokenize(document)
+            for index, token in enumerate(tokens):
+                # creo il nuovo termine
+                token = token + "$"
+
+                # devo ruotare la parola per fare la trailing wildcard
+                for i in token:
+                    # se è già presente nel dizionario faccio il merge delle posting list
+                    add(root1, token, docID, index)
+
+                    token = token[1:] + token[0]
+
+                # To observe the progress of our indexing.
+            if (docID % 1000 == 0 and docID != 0):
+                print(str(docID), end='...')
+                # enable break to limit indexing for testing
+                break
+
+        idx = cls()
+        idx._root = root1
+        idx._N = len(corpus)
+        # ritorno il dizionario con i termini ordinati in ordine alfabetico
+        return idx
+
+
 
 class IRsystem:
-
     def __init__(self, corpus, index):
         self._corpus = corpus
         self._index = index
@@ -260,17 +83,20 @@ class IRsystem:
         #print(index)
         return cls(corpus, index)
 
-    def answer_query_sc(self, words, connections):
-        norm_words = map(normalize, words)
+    def answer_query(self, words, connections):
+        '''da riabilitare'''
+        #norm_words = map(normalize, words)
+
+        norm_words = words
         postings = []
+
         for w in norm_words:
             try:
-                res = self._index[w]
+                res = find_prefix(self._index._root, w)
             except KeyError:
-                dictionary = [t.term for t in self._index._dictionary]
-                sub = find_nearest(w, dictionary, keep_first=True)
-                print("{} not found. Did you mean {}?".format(w, sub))
-                res = self._index[sub]
+                #implementare il not found
+                print("{} not found. Did you mean {}?")
+                pass
             postings.append(res)
         #having all the postings now I have to compute and-or-not
         plist = []
@@ -284,45 +110,29 @@ class IRsystem:
                 elif(conn == "or"):
                     plist = reduce(lambda x, y: x.union(y), postings)
                 else:
-                    plist = reduce(lambda x, y: x.notquery(), postings)
+                    pass
+                    '''sviluppare la not'''
+                    plist = reduce(lambda x, y: x.not_query(y), postings)
         return plist.get_from_corpus(self._corpus)
+        #plist = reduce(lambda x, y: x.phrase(y), postings)
+        return res.get_from_corpus(self._corpus)
 
-def edit_distance(u, v):
-    nrows = len(u) + 1
-    ncols = len(v) + 1
-    M = [[0] * ncols for i in range(0, nrows)]
-    for i in range(0, nrows):
-        M[i][0] = i
-    for j in range(0, ncols):
-        M[0][j] = j
-    for i in range(1, nrows):
-        for j in range(1, ncols):
-            candidates = [M[i-1][j] + 1, M[i][j-1] + 1]
-            if (u[i-1] == v[j-1]):
-                candidates.append(M[i-1][j-1])
-            else:
-                candidates.append(M[i-1][j-1] + 1)
-            M[i][j] = min(candidates)
-            # Remove the comments to print the distance matrix
-            # print(M[i][j], end="\t")
-        # print()
-    return M[-1][-1]  # Bottom right element of M
-
-
-def find_nearest(word, dictionary, keep_first=False):
-    if keep_first:
-        # If keep_first is true then we only search across the words
-        # in the dictionary starting with the same letter
-        dictionary = list(filter(lambda w: w[0] == word[0], dictionary))
-    # Remove comment to see the reduction in the size of the dictionary
-    # when keeping fixed the first letter
-    # print(len(dictionary))
-    # Apply f(x) = edit_distance(word, x) to all words in the dictionary
-    distances = map(lambda x: edit_distance(word, x), dictionary)
-    # Produce all the pairs (distance, term) usng zip and find one with
-    # the minimal distance.
-    return min(zip(distances, dictionary))[1]
-
+    def answer_phrase_query(self, words):
+        #da riabilitare
+        #norm_words = map(normalize, words)
+        norm_words = words
+        postings = []
+        for w in norm_words:
+            try:
+                res = find_prefix(self._index._root, w)
+            except KeyError:
+                #implementare il not found
+                print("{} not found. Did you mean {}?")
+                pass
+            postings.append(res)
+        #having all the postings now I have to check if they are near
+        plist = reduce(lambda x, y: x.phrase(y), postings)
+        return plist.get_from_corpus(self._corpus)
 
 def query(ir, text):
     words = text.split()
@@ -334,22 +144,35 @@ def query(ir, text):
         if (word == "and" or word == "or" or word == "not"):
             connections.append(word)
         else:
-            terms.append(word)
+            terms.append(word + "$")
 
-    answer = ir.answer_query_sc(terms, connections)
-    for movie in answer:
-        print(movie)
+    #answer = ir.answer_phrase_query(terms)
+    answer = ir.answer_query(terms,connections)
+    print(len(answer))
+    for doc in answer:
+        print(doc)
 
 #lettura file, creazione e salvataggio indice
 def initialization():
-    corpus = read_movie_descriptions()
+    tic = time.perf_counter()
+    corpus = read_data_descriptions()
     ir = IRsystem.from_corpus(corpus)
-    with open("index.pickle", "wb") as file_:
+    with open("data/trie.pickle", "wb") as file_:
         pickle.dump(ir, file_, -1)
+    toc = time.perf_counter()
+    print(f"Index created in {toc - tic:0.4f} seconds")
 
-#caricamento indice e query
 def operate():
-    ir = pickle.load(open("index.pickle", "rb", -1))
-    query(ir, "cat")
+    print("Retrieving index...")
+    ir = pickle.load(open("data/trie.pickle", "rb", -1))
+    print("Index retrieved!")
+    tic = time.perf_counter()
+    query(ir, "cat or space")
+    toc = time.perf_counter()
+    print(f"Query performed in {toc - tic:0.4f} seconds")
 
-operate()
+
+if __name__ == "__main__":
+    initialization()
+    #operate()
+
---
+++ term_operations.py
@@ -0,0 +1,198 @@
+import re
+from functools import total_ordering, reduce
+import nltk
+from nltk.corpus import stopwords
+
+#normalize e tokenize da sistemare con nltp
+def normalize(text):
+    """ A simple funzion to normalize a text.
+    It removes everything that is not a word, a space or an hyphen
+    and downcases all the text.
+    """
+    no_punctuation = re.sub(r'[^\w^\s^-]', '', text)
+    downcase = no_punctuation.lower()
+    return downcase
+
+
+def tokenize(movie):
+    """ From a movie description returns a posting list of all
+    tokens present in the description.
+    """
+    stop_words = set(stopwords.words('english'))
+    text = normalize(movie.description)
+    tokenized = nltk.word_tokenize(text)
+    filtered_sentence = [w for w in tokenized if not w in stop_words]
+
+    #return list(text.split())
+    return filtered_sentence
+
+@total_ordering
+class Posting:
+
+    def __init__(self, docID, pos):
+        self._docID = docID
+        self._poslist = []  # Term frequency
+        self._poslist.append(pos)
+
+    def get_from_corpus(self, corpus):
+        return corpus[self._docID]
+
+    def __eq__(self, other):
+        """Perform the comparison between this posting and another one.
+        Since the ordering of the postings is only given by their docID,
+        they are equal when their docIDs are equal.
+        """
+        return self._docID == other._docID
+
+    def __gt__(self, other):
+        """As in the case of __eq__, the ordering of postings is given
+        by the ordering of their docIDs
+        """
+        return self._docID > other._docID
+
+    def __repr__(self):
+        return str(self._docID) + " [" + str(self._poslist) + "]"
+
+    def __hash__(self):
+        return hash(self._docID)
+
+class PostingList:
+
+    def __init__(self):
+        self._postings = []
+
+    @classmethod
+    def from_docID(cls, docID, position):
+        """ A posting list can be constructed starting from
+        a single docID.
+        """
+        plist = cls()
+        plist._postings = [Posting(docID, position)]
+        return plist
+
+    @classmethod
+    def from_posting_list(cls, postingList):
+        """ A posting list can also be constructed by using another
+        """
+        plist = cls()
+        plist._postings = postingList
+        return plist
+
+    def merge(self, other):
+        #per unire le posting list di un termine presente in più docID
+        """Merge the other posting list to this one in a desctructive
+        way, i.e., modifying the current posting list. This method assume
+        that all the docIDs of the second list are higher than the ones
+        in this list. It assumes the two posting lists to be ordered
+        and non-empty. Under those assumptions duplicate docIDs are
+        discard
+        """
+        i = 0
+        last = self._postings[-1]
+        while (i < len(other._postings) and last == other._postings[i]):
+            last._poslist.append(other._postings[i]._poslist[0])
+            i += 1
+        self._postings += other._postings[i:]
+
+    def intersection(self, other):
+        #per la query
+        """Returns a new posting list resulting from the intersection
+        of this one and the one passed as argument.
+        """
+        intersection = []
+        i = 0
+        j = 0
+        while (i < len(self._postings) and j < len(other._postings)):
+            if (self._postings[i] == other._postings[j]):
+                intersection.append(self._postings[i])
+                i += 1
+                j += 1
+            elif (self._postings[i] < other._postings[j]):
+                i += 1
+            else:
+                j += 1
+        return PostingList.from_posting_list(intersection)
+
+    def union(self, other):
+        #per la query
+        """Returns a new posting list resulting from the union of this
+        one and the one passed as argument.
+        """
+        union = []
+        i = 0
+        j = 0
+        while (i < len(self._postings) and j < len(other._postings)):
+            if (self._postings[i] == other._postings[j]):
+                union.append(self._postings[i])
+                i += 1
+                j += 1
+            elif (self._postings[i] < other._postings[j]):
+                union.append(self._postings[i])
+                i += 1
+            else:
+                union.append(other._postings[j])
+                j += 1
+        for k in range(i, len(self._postings)):
+            union.append(self._postings[k])
+        for k in range(j, len(other._postings)):
+            union.append(other._postings[k])
+        return PostingList.from_posting_list(union)
+
+    '''da sviluppare la NOT QUERY PER TUTTO IL DIZIONARIO'''
+    def not_query(self, other):
+        '''per fare la not devo prendere la posting list del termine
+        se è chiamata singola allora devo prendere una posting list con tutti i termini
+        altrimenti basta togliere quelli che ci sono nell'altra'''
+
+        #self = quella della not
+        #other = altra posting list o posting list di tutti i documenti
+        not_term = []
+        i = 0
+        j = 0
+        while (i < len(self._postings) and j < len(other._postings)):
+            if (self._postings[i] == other._postings[j]):
+                #se sono uguali non lo aggiungo
+                i += 1
+                j += 1
+            elif (self._postings[i] < other._postings[j]):
+                not_term.append(self._postings[i])
+                i += 1
+            else:
+                j += 1
+        return PostingList.from_posting_list(not_term)
+
+    def phrase(self,other):
+        phrase = []
+        i=0
+        j=0
+        while (i < len(self._postings) and j < len(other._postings)):
+            x=0
+            y=0
+            if (self._postings[i] == other._postings[j]):
+                #sono nello stesso documento, devo controllare se sono vicini
+                while (x < len(self._postings[i]._poslist) and y < len(other._postings[j]._poslist)):
+                    if(self._postings[i]._poslist[x] == other._postings[j]._poslist[y]-1):
+                        phrase.append(other._postings[j])
+                    x += 1
+                    y += 1
+                i += 1
+                j += 1
+
+            elif (self._postings[i] < other._postings[j]):
+                i += 1
+            else:
+                j += 1
+        return PostingList.from_posting_list(phrase)
+
+    def get_from_corpus(self, corpus):
+        return list(map(lambda x: x.get_from_corpus(corpus), self._postings))
+
+
+class DataDescription:
+    #generare la coppia titolo-testo
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+    def __repr__(self):
+        return self.title  # + "\n" + self.description + "\n"
