478ead35c8906fb80f06f44aaad9132ac807c59f
Enrico Doretto <enricodoretto@MBP-di-Enrico-2.lan>
2020-12-13 09:50:38 +0100

    comment in english trie.py

--- trie.py
+++ trie.py
@@ -12,7 +12,7 @@ class TrieNode:
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-        # tenere traccia della parola corrente durante la ricerca
+        # keep track of the current word during the research
         self.word_list = []
 
     def insert(self, token, term):
@@ -23,14 +23,14 @@ class Trie:
             node = node.children[a]
         node.last = True
         try:
-            # se il nodo è già presente faccio il merge delle rispettive posting list
+            # if the node is already present merge the posting lists
             node.term.merge(term)
         except TypeError:
-            # se il nodo non esiste già
+            # if the node does not exist
             node.term = term
 
     def search(self, term):
-        # ricerca della parola non wildcard
+        # search of not wildcard word
         node = self.root
         found = True
         for a in list(term):
@@ -41,7 +41,7 @@ class Trie:
         if node.last:
             return node.term.posting_list
 
-    # scorro l'albero fino a trovare la wildcard
+    # scan the tree until the wildcard is found, then need to return all the children
     def get_wildcard(self, wcard):
         self.word_list = []
         node = self.root
@@ -60,15 +60,15 @@ class Trie:
         self.scan_wildcard_child(node, temp_word)
         return self.word_list
 
-    # una volta trovata la wildcard scorro i figli ricorsivamente e ritorno le posting list
+    # once the wildcard is found scan recursively its children and return their posting lists
     def scan_wildcard_child(self, node, word):
         if node.last:
             self.word_list.append(node.term.posting_list)
         for a, n in node.children.items():
             self.scan_wildcard_child(n, word + a)
 
-    # unica differenza dalla wildcard normale è che passo direttamente la regex della multiple
-    # e verifico direttamente se è il termine che soddisfa la wildcard semplice è da ritornare o no
+    # the only difference with the single wildcard is that with the multiple wildcard we need to pass the regex
+    # to check if the child of the simple wildcard matches the original query
     def get_multiple_wildcard(self, wcard, init):
         self.word_list = []
         node = self.root
@@ -91,6 +91,7 @@ class Trie:
 
     def scan_multiple_wildcard_child(self, node, word, init):
         reg = re.compile(init)
+        # the posting list of a child of the wildcard is returned only if the term matches the original query
         if node.last and re.match(reg, word):
             self.word_list.append(node.term.posting_list)
 
